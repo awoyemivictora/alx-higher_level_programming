a43781ea66f7f6d0c925e55b3e85b9242384d881
Awoyemi Victor Adewale <scoveyflashmite@gmail.com>
2022-10-02 15:45:18 +0000

    Creating more Data Structures: Set, Dictionary

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def weith_average(my_list=[]):
+
+	if my_list is None or my_list == []:
+		return 0
+	s_fx = sum(map(lambda x: x[0] * x[i], my_list))
+	s_f = sum(map(lambda y: y[1], my_list))
+	return s_fx /s_f
