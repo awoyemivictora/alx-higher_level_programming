58d19055ec5379a43558482ea891c04f043b7d2c
Awoyemi Victor Adewale <scoveyflashmite@gmail.com>
2022-10-03 00:08:53 +0000

    Creating more Data Structures: Set, Dictionary

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -2,11 +2,11 @@
 
 def complex_delete(a_dict, value):
 
-	if a_dict is None:
-		return None
-	del_key = None
-	keys = tuple(a_dict.keys())
-	for key in keys:
-		if a_dict[key] == value:
-			del a_dict[key]
-	return a_dict
+    if a_dict is None:
+        return None
+    del_key = None
+    keys = tuple(a_dict.keys())
+    for key in keys:
+        if a_dict[key] == value:
+            del a_dict[key]
+    return a_dict
