5de388d5b820dfe93ac06f00e046a2f21761a1b2
Awoyemi Victor Adewale <scoveyflashmite@gmail.com>
2023-01-14 22:43:57 +0000

    working with if/else

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -3,8 +3,11 @@ import random
 number = random.randint(-10, 10)
 
 if (number > 0):
+    print(number)
     print("is positive")
 elif (number == 0):
+    print(number)
     print("is zero")
 else:
+    print(number)
     print("is negative")
