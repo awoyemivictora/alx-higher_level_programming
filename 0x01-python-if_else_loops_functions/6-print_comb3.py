49ab6e04a444ea04f6fbfb6da689da39e0596d69
Awoyemi Victor Adewale <scoveyflashmite@gmail.com>
2023-01-15 23:50:10 +0000

    working with if/else statement

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-for num in range(9):
-	for var in range(num + 1, 10):
-		print("{:d}{:d}".format(num, var), end='\n' if num == 8 else ', ')
+
+for i in range (0, 100):
+    
+    print("{:02}".format(i), end=",")
+
+print("\n")
