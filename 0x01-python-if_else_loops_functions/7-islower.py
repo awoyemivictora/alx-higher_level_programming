79358c2f4d2594ef55797c539c71af5c897d14c3
Awoyemi Victor Adewale <scoveyflashmite@gmail.com>
2023-01-16 00:04:03 +0000

    working with if/else statement

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
+
+islower = __import__('7-islower').islower
+
 def islower(c):
-	return (ord(c) >= ord('a') and ord(c) <= ord('z'))
+    if(islower == True):
+        print("a is {}".format("lower" if islower("a") else "upper"))
+        print("H is {}".format("lower" if islower("H") else "upper"))
+        print("A is {}".format("lower" if islower("A") else "upper"))
+        print("3 is {}".format("lower" if islower("3") else "upper"))
+        print("g is {}".format("lower" if islower("g") else "upper"))
+
+        return True
