348c8c404f81673fa58996044cb03799ccc8aeb7
Awoyemi Victor Adewale <scoveyflashmite@gmail.com>
2023-01-16 10:41:13 +0000

    working with if/else statement

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -8,4 +8,5 @@ def uppercase(str):
         if ord(i) >= 97 and ord(i) <= 122:
             x = ord(i) - 32
             y = chr(x)
-            return y
+print('best")
+print("Best School 98 Battery street")
