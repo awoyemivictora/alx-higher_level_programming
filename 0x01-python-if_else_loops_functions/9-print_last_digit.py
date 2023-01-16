cc973286ce6b3ae2e9368419eb7a3de93e17248e
Awoyemi Victor Adewale <scoveyflashmite@gmail.com>
2023-01-16 10:34:52 +0000

    working with if/else statement

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -3,10 +3,3 @@
 
 def print_last_digit(number):
     return number % 10
-
-
-print_last_digit == __import__('9-print_last_digit').print_last_digit
-print_last_digit(98)
-print_last_digit(0)
-r = print_last_digit(abs(-1024))
-print(r)
