8781aeec10a28e3fc23f21da2f19bf739f9535e3
Awoyemi Victor Adewale <scoveyflashmite@gmail.com>
2023-01-16 21:52:56 +0000

    working with if/else

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,12 +2,12 @@
 
 
 def fizzbuzz():
-	for var in range(1, 101):
-		if var % 3 == 0 and var % 5 == 0:
-			print("FizzBuzz", end=' ')
-		elif var % 3 == 0:
-			print("Fizz", end=' ')
-		elif var % 5 == 0:
-			print("Buzz", end=' ')
-		else:
-			print(f"{var:d}", end=' ')
+    for var in range(1, 101):
+        if var % 3 == 0 and var % 5 == 0:
+            print("FizzBuzz", end=' ')
+	elif var % 3 == 0:
+	    print("Fizz", end=' ')
+	elif var % 5 == 0:
+	    print("Buzz", end=' ')
+	else:
+	    print(f"{var:d}", end=' ')
