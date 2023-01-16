2e6a581b7d6d346dd06ac17e96246e376802dd55
Awoyemi Victor Adewale <scoveyflashmite@gmail.com>
2023-01-16 07:34:25 +0000

    working with if/else statement

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -5,7 +5,5 @@ def uppercase(str):
     uppercase = __import__('8-uppercase').uppercase
 
     if (str == uppercase):
-        print("best")
+        print("best".format(uppercase))
         print("Best School 98 Battery street")
-    else:
-        continue
