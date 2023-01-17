5f2e090814984cef2ce481e966d6331b662b4f04
Awoyemi Victor Adewale <scoveyflashmite@gmail.com>
2023-01-17 14:34:15 +0000

    working with if/else

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -5,7 +5,7 @@ from calculator_1 import add, sub, mul, div
 a = 10
 b = 5
 
-print("a + b = ", add(a, b))
-print("a - b = ", sub(a, b))
-print("a * b = ", mul(a, b))
-print("a / b = ", div(a, b))
+print("a + b = ", ().format(add(a,b)))
+print("a - b = ".format(sub(a, b)))
+print("a * b = ".format(mul(a, b)))
+print("a / b = ".format(div(a, b)))
