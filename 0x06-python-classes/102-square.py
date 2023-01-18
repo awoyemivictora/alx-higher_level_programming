f33a514816ad682f21383289d2fa4bc11fecafa5
Awoyemi Victor Adewale <scoveyflashmite@gmail.com>
2023-01-18 16:46:29 +0000

    Working with python classes and objects

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,25 +1,21 @@
 #!/usr/bin/python3
-
 """Define a class Square."""
 
 
 class Square:
     """Represent a square."""
-    
+
     def __init__(self, size=0):
         """Initialize a new square.
-
         Args:
             size(int): The size of the new square.
         """
         self.size = size
 
-
     @property
     def size(self):
         """Get/set the current size of the square."""
         return (self.__size)
-
 
     @size.setter
     def size(self, value):
@@ -28,7 +24,6 @@ class Square:
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-
 
     def area(self):
         """Return the current area of the square."""
@@ -49,7 +44,7 @@ class Square:
     def __le__(self, other):
         """Define the <= comparison to a Square."""
         return self.area() <= other.area()
-    
+
     def __gt__(self, other):
         """Define the > comparison to a Square."""
         return self.area() > other.area()
