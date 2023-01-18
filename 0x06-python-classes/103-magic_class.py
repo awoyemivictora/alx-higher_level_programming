a3920172dd5265876ed65eb59477e5c220e4b9cd
Awoyemi Victor Adewale <scoveyflashmite@gmail.com>
2023-01-18 16:42:54 +0000

    Working with python classes and objects

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 """Define a MagicClass matching exactly a bytecode provided by Holberton."""
 
 import math
@@ -8,19 +7,15 @@ import math
 class MagicClass:
     """Represent a circle."""
 
-
     def __init__(self, radius=0):
         """Initialize a MagicClass.
-
         Arg:
             radius (float or int): The radius of the new MagicClass.
-
         """
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
             raise TypeError("radius must be a number")
         self.__radius = radius
-
 
     def area(self):
         """Return the area of the MagicClass."""
