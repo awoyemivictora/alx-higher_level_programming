6e552a5e39523ede5ee9a7e6a79498aab40a5b19
Awoyemi Victor Adewale <scoveyflashmite@gmail.com>
2023-01-19 10:44:03 +0000

    Working with TDD

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,2 +1,16 @@
 #!/usr/bin/python3
+"""Module containint a function to print first and last name"""
 
+
+def say_my_name(first_name, last_name=""):
+    """ prints first and last name
+        Arguments:
+            @first_name: first name to be printed
+            @second_name: last_name to be printed
+    """
+
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    if type(second)name) is not str:
+        raise TypeError("second_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
