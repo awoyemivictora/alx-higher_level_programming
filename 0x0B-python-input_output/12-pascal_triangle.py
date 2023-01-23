cfa7cd1a9a9d817c65e7a1d4c5d0167ef256fa7b
Awoyemi Victor Adewale <scoveyflashmite@gmail.com>
2023-01-23 06:25:24 +0000

    working with python input output

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -12,7 +12,7 @@ def pascal_triangle(n):
         return []
 
     triangles = [[1]]
-    while len(triangle) != n:
+    while len(triangles) != n:
         tri = triangles[-1]
         tmp = [1]
         for i in range(len(tri) - 1):
