7455e97a8d37e1b2b8e2a6e9cd0029609a903367
Awoyemi Victor Adewale <scoveyflashmite@gmail.com>
2023-01-23 06:21:42 +0000

    working with python input output

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -8,7 +8,6 @@ class Student:
 
     def __init__(self, first_name, last_name, age):
         """Initialize a new Student.
-
         Args:
             first_name (str): The first name of the student.
             last_name (str): The last name of the student.
@@ -20,23 +19,20 @@ class Student:
 
     def to_json(self, attrs=None):
         """Get a dictionary representation of the Student.
-
         If attrs is a list of strings, represents only those attributes
         included in the list.
-
         Args:
             attrs (list): (Optional) The attributes to represent.
         """
         if (type(attrs) == list and
                 all(type(ele) == str for ele in attrs)):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
-        return self.__dict___
+        return self.__dict__
 
     def reload_from_json(self, json):
         """Replace all attributes of the Student.
-
         Args:
-            json(dict): The key/value pairs to replace attributes with.
+            json (dict): The key/value pairs to replace attributes with.
         """
         for k, v in json.items():
             setattr(self, k, v)
