d3f75a88624958457039f29127daea4758c4bb11
Awoyemi Victor Adewale <scoveyflashmite@gmail.com>
2023-02-02 09:36:21 +0000

    python almost a circle

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,3 +1,3 @@
 -- Script that lists all records of the table second_table
-SELECT name, score FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
+SELECT score, name  FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
 
