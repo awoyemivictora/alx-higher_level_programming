75952f4000ae2bb4517e5650da14d3c56220ceb1
Awoyemi Victor Adewale <scoveyflashmite@gmail.com>
2023-02-02 02:22:35 +0000

    python almost a circle

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,5 +1,5 @@
 -- Creating a table called first_table in the current MySQL database server
-CREATE TABLE first_table IF NOT EXISTS(
+CREATE TABLE first_table IF EXISTS(
 	id INT,
 	name VARCHAR(256)
 );
