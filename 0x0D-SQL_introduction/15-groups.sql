8b4e62775bbb7918c292941abe44f49e8a02590d
Awoyemi Victor Adewale <scoveyflashmite@gmail.com>
2023-02-02 09:10:12 +0000

    python almost a circle

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,3 +1,3 @@
 -- Script that lists the number of records with the same score in the table second_table
-SELECT score, COUNT(*) as c FROM some_table ORDER BY score;
+SELECT score, COUNT(*) as c FROM some_table GROUP BY score;
 
