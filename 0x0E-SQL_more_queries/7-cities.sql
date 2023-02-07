f1ef9a8e4147b239bb0e79e0811bb53ca269cd33
Awoyemi Victor Adewale <scoveyflashmite@gmail.com>
2023-02-07 16:46:15 +0000

    Working with SQL - More queries

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,7 +1,10 @@
 -- Script that creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa) on your MySQL server
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
-CREATE TABLES IF NOT EXISTS `cities`(
-	id INT UNIQUE NOT NULL AUTO INCREMENT PRIMARY KEY,
-	state_id INT NOT NULL (states = id),
-	name VARCHAR(256) NOT NULL
+CREATE TABLES IF NOT EXISTS `hbtn_0d_usa`.`cities`(
+	PRIMARY KEY(`id`),
+	`id` 		INT NOT NULL AUTO INCREMENT,
+	`state_id` 	INT NOT NULL (states = id),
+	`name`		VARCHAR(256) NOT NULL,
+	FOREIGN KEY(`state_id`)
+	REFERENCES `hbtn_0d_usa`.`states`(`id`)
 	);
