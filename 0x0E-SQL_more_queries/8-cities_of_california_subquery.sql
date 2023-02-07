35d9899975ee01e01bb1a92197a6dadab461e170
Awoyemi Victor Adewale <scoveyflashmite@gmail.com>
2023-02-07 16:36:59 +0000

    Working with SQL - More queries

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,3 @@
 -- Script that lists all the cities of California that can be found in the database hbtn_0d_usa
-SELECT id, name as c FROM states ORDER BY c ASC;
+SELECT * FROM states ORDER BY name ASC;
+
