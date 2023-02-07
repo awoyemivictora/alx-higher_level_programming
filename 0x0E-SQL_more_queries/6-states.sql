b94f1777892e6da43568d6b5d19f90cd229e0843
Awoyemi Victor Adewale <scoveyflashmite@gmail.com>
2023-02-07 14:09:17 +0000

    Working with SQL - More queries

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
 -- Script that creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on your MySQL server
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
 CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states`(
-	id INT UNIQUE NOT NULL AUTO INCREMENT PRIMARY KEY,
-	name VARCHAR(255) NOT NULL,
+	`id` INT UNIQUE NOT NULL AUTO INCREMENT PRIMARY KEY,
+	`name` VARCHAR(255) NOT NULL,
 );
