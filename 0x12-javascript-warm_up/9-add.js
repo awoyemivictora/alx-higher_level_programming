6909068d7e383d738aaaf2613e0229f67e327791
Awoyemi Victor Adewale <scoveyflashmite@gmail.com>
2023-02-21 04:02:47 +0100

    JavaScript - Warm Up

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-function add(a, b) {
-	sum = a + b
-	console.log(sum);
+function add (a, b) {
+  const sum = a + b;
+  console.log(sum);
 }
 
 add(Number(process.argv[2]), Number(process.argv[3]));
