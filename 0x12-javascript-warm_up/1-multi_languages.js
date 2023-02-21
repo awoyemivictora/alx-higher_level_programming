55097d3cdfa0100bb18dd233740f004d296e6f90
Awoyemi Victor Adewale <scoveyflashmite@gmail.com>
2023-02-21 03:48:40 +0100

    JavaScript - Warm Up

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-let a = "C is fun"
-let b = "Python is cool"
-let c = "JavaScript is amazing"
+const a = 'C is fun';
+const b = 'Python is cool';
+const c = 'JavaScript is amazing';
 
 console.log(a);
 console.log(b);
