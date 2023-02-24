ccffd8219c8736f08cb0af443896963eb1ebc39a
Awoyemi Victor Adewale <scoveyflashmite@gmail.com>
2023-02-24 11:13:49 +0100

    Working with javascript objects, scopes and closures

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-class Rectangle {{
-  constructor (w, h) {
+class Rectangle {
+  constructor(w, h) {
     this.width = w;
     this.height = h;
   }
--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 
 class Rectangle {
-  constructor (w, h) {
-    if ((w > 0) && (h > 0)) {
+  constructor(w, h) {
+    if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
   }
 
-  print () {
+  print() {
     for (let i = 0; i < this.height; i++) {
       const s = '';
       for (let j = 0; j < this.width; j++) {
