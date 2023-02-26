1b5de2bcfe1ad905f32f9a9719d721abefedfa83
Awoyemi Victor Adewale <scoveyflashmite@gmail.com>
2023-02-26 09:45:26 +0100

    Working on JavaScript - Web scraping

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,3 +1,13 @@
 #!/usr/bin/node
 
+/* File System Object */
+const fs = require('fs');
 
+/* Read File */
+fs.readFile('3-starwars_title.js', bar);
+
+function bar(err, data) {
+  err
+    ? Function('error', 'throw error')(err)
+    : console.log(JSON.stringify(data));
+}
