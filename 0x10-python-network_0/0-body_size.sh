c6394a5f80547342f4e45996d03bfd7c92405641
Awoyemi Victor Adewale <scoveyflashmite@gmail.com>
2023-03-17 02:37:00 +0100

    Working with Python network #0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a request to that URL and displays the size of the body of the response
+
+curl 0.0.0.0:5000
---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+# Write a function that finds a peak in a list of unsorted integers.
+
